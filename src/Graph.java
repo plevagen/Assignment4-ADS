402f21fac21da28afc974ed4605ded2c04b174c8
plevagen <varkginglouh@gmail.com>
2026-05-15 12:25:55 +0500

    Added a vertex, edges and graph file

--- src/Graph.java
+++ src/Graph.java
@@ -1,2 +1,30 @@
+import java.util.*;
+
 public class Graph {
+    private ArrayList<Vertex> vertices;
+    private ArrayList<ArrayList<Integer>> adjacencyList;
+
+    public Graph(){
+        vertices = new ArrayList<>();
+        adjacencyList = new ArrayList<>();
+    }
+
+    public void addVertex(Vertex v){
+        vertices.add(v);
+        adjacencyList.add(new ArrayList<>());
+    }
+
+    public void addEdge(int from, int to){
+        if(from < vertices.size() && to < vertices.size()){
+            adjacencyList.get(from).get(to);
+        }
+    }
+
+    public void printGraph(){
+        System.out.println("Graph Structure");
+        for(int i = 0; i < vertices.size(); i++){
+            System.out.print("Vertex " + i + "->");
+            System.out.println(adjacencyList.get(i));
+        }
+    }
 }
