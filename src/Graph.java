73aeb97791392b3be761abe4c2bcb1ad5ce4bd51
plevagen <varkginglouh@gmail.com>
2026-05-15 12:42:20 +0500

    .

--- src/Graph.java
+++ src/Graph.java
@@ -4,27 +4,72 @@ public class Graph {
     private ArrayList<Vertex> vertices;
     private ArrayList<ArrayList<Integer>> adjacencyList;
 
-    public Graph(){
+    public Graph() {
         vertices = new ArrayList<>();
         adjacencyList = new ArrayList<>();
     }
 
-    public void addVertex(Vertex v){
+    public void addVertex(Vertex v) {
         vertices.add(v);
         adjacencyList.add(new ArrayList<>());
     }
 
-    public void addEdge(int from, int to){
-        if(from < vertices.size() && to < vertices.size()){
+    public void addEdge(int from, int to) {
+        if (from < vertices.size() && to < vertices.size()) {
             adjacencyList.get(from).get(to);
         }
     }
 
-    public void printGraph(){
+    public void printGraph() {
         System.out.println("Graph Structure");
-        for(int i = 0; i < vertices.size(); i++){
+        for (int i = 0; i < vertices.size(); i++) {
             System.out.print("Vertex " + i + "->");
             System.out.println(adjacencyList.get(i));
         }
+    }
+
+    public void bfs(int start) {
+        boolean[] visited = new boolean[vertices.size()];
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[start] = true;
+        queue.add(start);
+
+        System.out.print("BFS traversal: ");
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            System.out.println(cur + " ");
+
+            for (int neighbor : adjacencyList.get(cur)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public void dfs(int start) {
+        boolean[] visited = new boolean[vertices.size()];
+        System.out.println("DFS traversal");
+        dfsHelper(start, visited);
+        System.out.println();
+    }
+
+    private void dfsHelper(int cur, boolean[] visited) {
+        visited[cur] = true;
+        System.out.println(cur + " ");
+
+        for (int neighbor : adjacencyList.get(cur)) {
+            if (!visited[neighbor]) {
+                dfsHelper(neighbor, visited);
+            }
+        }
+    }
+
+    public int getSize(){
+        return vertices.size();
     }
 }
